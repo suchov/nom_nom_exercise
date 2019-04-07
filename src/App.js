6267201f82fd364521cc1c6f8deb57eff347a6b4
suchov <artem.suchov@gmail.com>
2019-04-07 19:43:51 +0300

    add nav shadow move to 2 spaces tab to be consistent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <Header />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
---
+++ src/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const  Header = () => {
+  return (
+    <div className="header-div">
+      <header>
+
+      </header>
+    </div>
+  )
+}
+
+
+export default Header;
