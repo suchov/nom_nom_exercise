05d9db3d562c5b20ad3ed832e97164e486f25cd2
suchov <artem.suchov@gmail.com>
2019-04-07 20:07:33 +0300

    add logo image/url

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Header.css';
+import logo from './logo_main.svg';
 
 const  Header = () => {
   return (
     <div className="header-div">
       <header>
-
+        <a href="/">
+          <img className="header-nav--logo" src={logo} alt="Fresh dog food delivery, best dog food - NomNomNow"/>
+        </a>
       </header>
     </div>
   )
