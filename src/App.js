a361ba668d16a5b453d651f0e00185a5c541aef0
suchov <artem.suchov@gmail.com>
2019-04-08 10:31:39 +0300

    adding side menu with some styling and update body styles

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
--- src/header/Header.js
+++ src/header/Header.js
@@ -3,12 +3,34 @@ import './Header.css';
 import logo from './logo_main.svg';
 
 const  Header = () => {
+  console.log('WOW WOW WOW');
+  
   return (
     <div className="header-div">
       <header>
         <a href="/">
           <img className="header-nav--logo" src={logo} alt="Fresh dog food delivery, best dog food - NomNomNow"/>
         </a>
+        <ul className="menu">
+          <li className="dropdown">
+            <a href="#/">Dogs</a>
+            <span className="down-arrow">▾</span>
+          </li>
+          <li className="dropdown">
+            <a href="#/">Cats</a>
+            <span className="down-arrow">▾</span>
+          </li>
+          <li className="dropdown">
+            <a href="#/">Learn</a>
+            <span className="down-arrow">▾</span>
+          </li>
+          <li>
+            <a href="#/">Log in</a>
+          </li>
+          <li>
+            <a href="/profile" className="cta-button">20% off trial</a>
+          </li>
+        </ul>
       </header>
     </div>
   )
