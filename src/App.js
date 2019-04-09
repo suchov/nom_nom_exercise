162a92602ba0abc3a6310eb16ca6761e5eb4c26a
suchov <artem.suchov@gmail.com>
2019-04-09 09:42:21 +0300

    add hiding menu and link to fonts, fix classes for React

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <link type='text/css' href='http://fonts.googleapis.com/css?family=Lato:400,700' />
         <Header />
         
       </div>
--- src/header/Header.js
+++ src/header/Header.js
@@ -3,26 +3,25 @@ import './Header.css';
 import logo from './logo_main.svg';
 
 const  Header = () => {
-  console.log('WOW WOW WOW');
-  
   return (
     <div className="header-div">
       <header>
         <a href="/">
           <img className="header-nav--logo" src={logo} alt="Fresh dog food delivery, best dog food - NomNomNow"/>
         </a>
+        <label className="menu-icon" htmlFor="menu-button"><span className="navicon"></span></label>
         <ul className="menu">
           <li className="dropdown">
             <a href="#/">
               Dogs
               <span className="down-arrow">▾</span>
             </a>
-            <div class="dropdown-content">
-              <a href="/fresh-dog-food-recipes" class="">Recipes</a>
-              <a href="/site/products/dog-food-variety-pack" class="">Variety Pack</a>
-              <a href="/site/products/dog-treats/" class="">Treats</a>
-              <a href="/site/products/dog-supplements/" class="">Supplements</a>
-              <a href="/site/products/microbiome/" class="">Microbiome</a>
+            <div className="dropdown-content">
+              <a href="/fresh-dog-food-recipes" className="">Recipes</a>
+              <a href="/site/products/dog-food-variety-pack" className="">Variety Pack</a>
+              <a href="/site/products/dog-treats/" className="">Treats</a>
+              <a href="/site/products/dog-supplements/" className="">Supplements</a>
+              <a href="/site/products/microbiome/" className="">Microbiome</a>
             </div>
           </li>
           <li className="dropdown">
@@ -30,12 +29,12 @@ const  Header = () => {
               Cats
               <span className="down-arrow">▾</span>
             </a>
-            <div class="dropdown-content">
-              <a href="/fresh-cat-food-recipes" class="">Recipes</a>
-              <a href="/site/products/cat-food-variety-pack" class="">Variety Pack</a>
-              <a href="/site/products/dog-treats/?pet_type=CAT" class="">Treats</a>
-              <a href="/site/products/cat-supplements/" class="">Supplements</a>
-              <a href="/site/products/microbiome/?pet_type=CAT" class="">Microbiome</a>
+            <div className="dropdown-content">
+              <a href="/fresh-cat-food-recipes" className="">Recipes</a>
+              <a href="/site/products/cat-food-variety-pack" className="">Variety Pack</a>
+              <a href="/site/products/dog-treats/?pet_type=CAT" className="">Treats</a>
+              <a href="/site/products/cat-supplements/" className="">Supplements</a>
+              <a href="/site/products/microbiome/?pet_type=CAT" className="">Microbiome</a>
             </div>
           </li>
           <li className="dropdown">
@@ -43,10 +42,10 @@ const  Header = () => {
               Learn
               <span className="down-arrow">▾</span>
             </a>
-            <div class="dropdown-content">
-              <a href="/site/how-it-works" class="">Our approach</a>
-              <a href="/learn/success-stories" class="">Success stories</a>
-              <a href="/learn" class="">Articles</a>
+            <div className="dropdown-content">
+              <a href="/site/how-it-works" className="">Our approach</a>
+              <a href="/learn/success-stories" className="">Success stories</a>
+              <a href="/learn" className="">Articles</a>
             </div>
           </li>
           <li>
